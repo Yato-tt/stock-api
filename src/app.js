a7200ed9591e4196d4a01e134523074f1b20c2db
Yato-tt <victor.yatosekkie@gmail.com>
2026-06-04 01:38:53 -0300

    correção listagem de itens no SSR

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'cache-control', 'Pragma'],
   credentials: true
 }));
 
